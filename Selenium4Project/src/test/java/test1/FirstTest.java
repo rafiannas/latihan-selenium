ac4a5a2b770874a2aceb32dbf45d3ec581ff9848
rafi <rafi.annas2000@gmail.com>
2023-01-26 21:57:12 +0700

    Merge branch 'master' of https://github.com/rafiannas/latihan-selenium.git

--- Selenium4Project/src/test/java/test1/FirstTest.java
+++ Selenium4Project/src/test/java/test1/FirstTest.java
@@ -1,10 +1,13 @@
 package test1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
 
 	}
 
